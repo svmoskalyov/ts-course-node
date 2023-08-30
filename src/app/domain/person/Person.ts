278ffe5b356962debfcc1999e9bba95e27089120
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-08-30 12:03:38 +0300

    Structure of the answer

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -2,28 +2,38 @@ import { JsonController, Get, Post, Body, Param } from "routing-controllers";
 
 import { IPerson } from "./Person.types";
 
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
+
 const storeData: IPerson[] = [];
 
 @JsonController("/person")
 export default class Person {
   @Get()
   async getAll() {
-    return storeData;
+    return new ApiResponse(true, storeData);
   }
 
   @Get("/:id")
-  async getOne(@Param("id") id: number): Promise<IPerson | {}> {
+  async getOne(@Param("id") id: number): Promise<ApiResponse<IPerson | {}>> {
     const person = storeData.find((item) => {
       return item.id === id;
     });
 
-    return person || {};
+    if (!person) {
+      throw new ApiError(404, {
+        code: "PERSON_NOT_FOUND",
+        message: `Person with id ${id} not found`,
+      });
+    }
+
+    return new ApiResponse(true, person);
   }
 
   @Post()
   async setPerson(@Body() body: IPerson) {
     storeData.push(body);
 
-    return true;
+    return new ApiResponse(true, "Person successfully created");
   }
 }
