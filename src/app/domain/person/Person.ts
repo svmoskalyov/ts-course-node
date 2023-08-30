dddaab981b860a0aab0fd77e8abbfb96864bbb13
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-08-30 12:09:52 +0300

    Data Validation

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,6 +1,8 @@
 import { JsonController, Get, Post, Body, Param } from "routing-controllers";
+import { validate } from "class-validator";
 
 import { IPerson } from "./Person.types";
+import { CreatePerson } from "./CreatePerson.dto";
 
 import { ApiResponse } from "helpers/ApiResponse";
 import { ApiError } from "helpers/ApiError";
@@ -31,8 +33,21 @@ export default class Person {
   }
 
   @Post()
-  async setPerson(@Body() body: IPerson) {
-    storeData.push(body);
+  async setPerson(@Body() body: CreatePerson) {
+    // validate the body using class-validator
+    const errors = await validate(body);
+
+    if (errors.length > 0) {
+      throw new ApiError(400, {
+        message: "Validation failed",
+        code: "PERSON_VALIDATION_ERROR",
+        errors,
+      });
+    }
+
+    const id = storeData.length;
+
+    storeData.push({ ...body, id });
 
     return new ApiResponse(true, "Person successfully created");
   }
